bff0209ea7558cfc217a1bb1b567612a0b49524f
mcurtisCMNH <81580020+mcurtisCMNH@users.noreply.github.com>
2023-12-03 17:50:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React'];
+  var member = {
+    name: 'Rafael',
+    age: 25,
+    skills: skills
+  };
+  console.log(member);
+  console.log(member.skills[3]);
+}
